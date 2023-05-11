b4708e34cb68d1f7227ae82147f7bb250e263aa8
Sidhant_sarangi2 <sidhantsarangi2@gmail.com>
2023-05-10 21:06:36 -0700

    added a link,wrote header,controlled news flow,controlled no of news

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 from send_email import gmailer
+topic = "tesla"
 api_key = "808afafda5b549b5a3e6fe62b8e381f4"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-11&sortBy=publishedAt&apiKey=" \
-      "808afafda5b549b5a3e6fe62b8e381f4"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-04-11&" \
+      "sortBy=publishedAt&" \
+      "apiKey=" \
+      "808afafda5b549b5a3e6fe62b8e381f4&" \
+      "language=en"
 request = requests.get(url)
 content = request.json()
 body = ""
-for article in content["articles"]:
-    body = body + article['title'] + "\n" + article['description'] + 2*'\n'
+for article in content["articles"][:15]:
+    body ="news of the day:" + "\n" + body + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*'\n'
 body = body.encode('utf-8')
 gmailer(body)
 
